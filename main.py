f650d662bfafa20f6b969a28ae47b44c70b803d8
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-22 00:02:21 +0900

    added utils

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import logging
 import sys
 import os
 
-from functions.get_data_prefectures import get_simple_json
-from functions.json_utils import json_read, json_write
+from utils.get_data_prefectures import get_simple_json
+from utils.json_utils import json_read, json_write
 
 formatter = '%(levelname)s : %(asctime)s : %(message)s'
 logging.basicConfig(level=logging.ERROR, format=formatter)
 
 
-from functions.get_data_prefectures import get_requests
+from utils.get_data_prefectures import get_requests
 
 if __name__ == '__main__':
 
@@ -19,36 +19,43 @@ if __name__ == '__main__':
     except Exception as e:
         sys.exit(logging.error('%s', 'Failed to call API'))
 
-    overall_stats = {}
-    for i in range(len(body)):
-        get_simple_json(body[i], overall_stats)
+    # overall_stats = {}
+    # for i in range(len(body)):
+    #     get_simple_json(body[i], overall_stats)
 
-    print(overall_stats)
 
-    # directory = os.path.dirname(__file__)
-    # print(directory)
-    # save_dir = os.path.join(directory, 'saves')
-    # print(save_dir)
-    # if not os.path.isdir(save_dir):
-    #     os.makedirs(save_dir)
-    #
-    # save_file_path = os.path.join(save_dir, 'save.json')
-    #
-    #
+
+
+    directory = os.path.dirname(__file__)
+
+    save_dir = os.path.join(directory, 'saves')
+
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    daily_infections = os.path.join(save_dir, 'daily.json')
+    save_file_path = os.path.join(save_dir, 'save.json')
+    prefecture_stats = get_simple_json(body)
+
     # if os.path.isfile(save_file_path):
     #     old_body = json_read(save_file_path)
-    #     # old_day = old_body['date']
-    #     # difference = body['positive'] - old_body['positive']
+    #     old_day = old_body['date']
+
+    # for loop 書かないと
+    #     difference = body['positive'] - old_body['positive']
     # else:
     #     old_day = None
     #     difference = 0
+
+    json_write(prefecture_stats, save_file_path)
 
     # if day != old_day:
     #     if os.path.isfile(daily_infections):
     #         daily = json_read(daily_infections)
     #     else:
     #         daily = []
-    #
+
+
     #     day_obj = datetime.datetime.strptime(str(day), r'%Y%m%d')
     #     daily.append({
     #         'date': day,
