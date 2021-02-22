3d6e6a42ed6d18128ae5e8ca42bfa68d6b0f4c04
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-23 00:07:50 +0900

    added df_conversion

---
+++ analysis/total_stats_analysis.py
@@ -0,0 +1,19 @@
+import datetime
+import pandas as pd
+
+def convert_total_stats_to_df(db_stacks):
+    data = db_stacks.find()[0]
+    data = data['stats']
+
+    temp_data_list = []
+    df = pd.DataFrame(columns=['date', 'pcr_cases', 'positive', 'severe', 'death', 'deaths_rate'])
+
+    for i in range(len(data)):
+        s_datetime = datetime.datetime.strptime(str(data[i]['date']), '%Y%m%d')
+        data[i]['date'] = s_datetime
+        temp_data_list.append(data[i])
+
+    df = df.append(temp_data_list, ignore_index=True)
+
+    return df
+
