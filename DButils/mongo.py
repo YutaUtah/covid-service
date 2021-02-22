9a49797d17dde878c6e106633e8f40cb30e8b1a2
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-22 20:38:14 +0900

    created db_instantiate()

--- DButils/mongo.py
+++ DButils/mongo.py
@@ -1,4 +1,3 @@
-import datetime
 from pymongo import MongoClient
 import os
 import logging
@@ -31,38 +30,39 @@ def show_content(db_stacks):
     for stack in db_stacks.find():
         print(stack)
 
+def db_instantiate(json_dir, db_name):
+    save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
+    save_file_path = os.path.join(save_dir, json_dir)
+    db = client[db_name]
+    db_stacks = db.stacks
+
+    return db_stacks, save_file_path
+
 
 def update_mongodb(json):
     if len(json.keys()) == 2:
-        save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
-        save_file_path = os.path.join(save_dir, 'latest_by_prefecture.json')
-        db = client['prefecture_database']
-        db_stacks_by_prefecture = db.stacks
+        db_stacks, save_file_path = db_instantiate('latest_by_prefecture.json', 'prefecture_database')
         if not os.path.isfile(save_file_path):
             logger.error('no file is found')
         else:
-            if db_stacks_by_prefecture.find_one({'date': json['date']}):
+            if db_stacks.find_one({'date': json['date']}):
                 pass
             else:
-                db_stacks_by_prefecture.insert_one(json).inserted_id
+                db_stacks.insert_one(json).inserted_id
         print('prefecture_database: ')
-        show_content(db_stacks_by_prefecture)
+        show_content(db_stacks)
 
     elif len(json.keys()) == 1:
-        save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
-        save_file_path = os.path.join(save_dir, 'latest_total_stats.json')
-        db = client['total_stats_database']
-        db_stacks_total_stats = db.stacks
+        db_stacks, save_file_path = db_instantiate('latest_total_stats.json', 'total_stats_database')
         if not os.path.isfile(save_file_path):
             logger.error('no file is found')
         else:
-            if db_stacks_total_stats.find_one({}, {'date': json['stats'][-1]['int_date']}):
+            if db_stacks.find_one({}, {'date': json['stats'][-1]['int_date']}):
                 pass
             else:
-
-                db_stacks_total_stats.insert_one(json).inserted_id
+                db_stacks.insert_one(json).inserted_id
         print('total stats database: ')
-        show_content(db_stacks_total_stats)
+        show_content(db_stacks)
 
 
 
