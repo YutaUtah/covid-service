7fc630a8f62e3a341dd92d488423c1704cce2650
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-22 20:55:00 +0900

    craeted constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+class Const(object):
+    BY_PREFECTURE_API = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures'
+    TOTAL_STATS_API = 'https://covid19-japan-web-api.now.sh/api/v1/total?history=true'
