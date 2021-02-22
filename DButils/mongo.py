ae234a2f7270252f0dabcfbfb254889161930ca3
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-22 19:40:22 +0900

    Modified mongo.py for more data source

--- DButils/mongo.py
+++ DButils/mongo.py
@@ -21,24 +21,52 @@ def delete_one_stack_mongodb(stack):
     db_stacks.delete_one(stack)
 
 
-def delete_all_stack_mongodb():
+def delete_all_stack_mongodb(db_stacks):
     for stack in db_stacks.find():
         db_stacks.delete_one(stack)
+        print(stack)
 
-def update_mongodb(daily_json):
-    save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
-    save_file_path = os.path.join(save_dir, 'latest.json')
-    if not os.path.isfile(save_file_path):
-        logger.error('no file is found')
-    else:
-        if db_stacks.find_one({'date': daily_json['date']}):
-            pass
-        else:
-            db_stacks.insert_one(daily_json).inserted_id
 
-def show_content():
+def show_content(db_stacks):
     for stack in db_stacks.find():
         print(stack)
 
+
+def update_mongodb(json):
+    if len(json.keys()) == 2:
+        save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
+        save_file_path = os.path.join(save_dir, 'latest_by_prefecture.json')
+        db = client['prefecture_database']
+        db_stacks_by_prefecture = db.stacks
+        if not os.path.isfile(save_file_path):
+            logger.error('no file is found')
+        else:
+            if db_stacks_by_prefecture.find_one({'date': json['date']}):
+                pass
+            else:
+                db_stacks_by_prefecture.insert_one(json).inserted_id
+        print('prefecture_database: ')
+        show_content(db_stacks_by_prefecture)
+
+    elif len(json.keys()) == 1:
+        save_dir = os.path.join(os.path.dirname('covidAPI'), 'saves')
+        save_file_path = os.path.join(save_dir, 'latest_total_stats.json')
+        db = client['total_stats_database']
+        db_stacks_total_stats = db.stacks
+        if not os.path.isfile(save_file_path):
+            logger.error('no file is found')
+        else:
+            if db_stacks_total_stats.find_one({}, {'date': json['stats'][-1]['int_date']}):
+                pass
+            else:
+
+                db_stacks_total_stats.insert_one(json).inserted_id
+        print('total stats database: ')
+        show_content(db_stacks_total_stats)
+
+
+
+
 # delete_all_stack_mongodb()
-# show_content()
+# show_content(db_stacks_by_prefecture)
+# show_content(db_stacks_total_stats)
